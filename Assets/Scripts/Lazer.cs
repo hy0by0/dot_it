de5165c176713303e9faa77d0aabece13ac78b89
hy0by0 <hy0by02792c3@gmail.com>
2025-11-04 18:34:48 +0900

    LazerStarterとLazerスクリプト変更
    ひとまずlazerStarterとlazerのスクリプトを大幅変更。それぞれについての内容を後でちゃんとメモしておく

--- Assets/Scripts/Lazer.cs
+++ Assets/Scripts/Lazer.cs
@@ -1,211 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.SocialPlatforms.Impl;
-using static TreeEditor.TreeGroup;
+using UnityEngine.UI;
 
 public class Lazer : MonoBehaviour
 {
-    LineRenderer lineRenderer;
-    EdgeCollider2D edgeCollider;
+    [SerializeField] private GameObject lazerPrefab;
+    const float MAX_DISTANCE = 23.0f;  //箱の対角線の長さ（レーザーの最大長を設定）
+    const int REFLECT_NUM = 3;  //反射回数
+    private GameObject preWall;
 
-    public Transform startPos;
-    public GameObject endPosObj;
-    public Transform endPos;
-    EdgeLine edgeLine;
-
-    public float tongueSpeed = 1.0f;
-    public float maxMagnitude = 2.0f;
-    public float canMagnitude;
-
-    public float tongueScore = 0.0f;
-    public int getCount = 0;
-
-    //Vector3[] positions;
-    private Vector3 currentDirection; //現在の伸びていく方向を保持するためのベクトル変数
-    private List<Vector3> linePoints = new List<Vector3>(); //頂点座標の管理するリスト。始点・終点・折れ曲がった点座標をリストで管理
-
-
-    public bool moving = false; //伸びるかどうかのフラグ
-    public bool backFlag = false; //縮むかどうかのフラグ
-
-
-    void Start()
+    /**
+     * レーザー生成関数
+     * @param {Vector3} origin - レーザーの原点
+     * @param {Vector3} direction - レーザーの方向
+     * @param {int} n - 何本目か(0はじまり)
+    **/
+    public void creat(Vector3 origin, Vector2 direction, int n)
     {
-        // LineRendererコンポーネントをゲームオブジェクトにアタッチする
-        lineRenderer = this.gameObject.GetComponent<LineRenderer>();
-        edgeCollider = this.gameObject.GetComponent<EdgeCollider2D>();
-        endPos = endPosObj.GetComponent<Transform>();
-        edgeLine = endPosObj.GetComponent<EdgeLine>();
-
-        // 初期化
-        linePoints.Add(startPos.position);
-        linePoints.Add(endPos.position);
-
-        currentDirection = (endPos.position - startPos.position).normalized;
-        UpdateLine();
-        canMagnitude = maxMagnitude;
-
-
-        // 点の数を指定する
-        //lineRenderer.positionCount = positions.Length;
-
-        // 線を引く場所を指定する
-        //lineRenderer.SetPositions(positions);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //クリック中のマウスへの方向ベクトル　currentVectorを更新させて！！！！
-        if (Input.GetMouseButton(0))
+        if (n == 0) preWall = null;
+        if (n < REFLECT_NUM + 1)
         {
-            moving = true;
-            currentDirection = endPos.up;
+            GameObject lazerChild = Instantiate(lazerPrefab, this.transform);
+            lazerChild.name = "lazer_" + n;
+            LineRenderer line = lazerChild.GetComponent<LineRenderer>();
+            RaycastHit2D[] hits = Physics2D.RaycastAll(origin, direction, MAX_DISTANCE);
 
-        }
-        else if(Input.GetMouseButtonUp(0))
-        {
-            moving = false;
-            backFlag = true;
-        }
-
-        if (moving) //伸びていくとき
-        {
-            int layerMask = 1 << LayerMask.NameToLayer("Wall");
-            RaycastHit2D hit = Physics2D.Raycast(endPos.position, currentDirection, 0.5f, layerMask);
-            Debug.DrawRay(endPos.position, currentDirection * 0.5f, Color.green);
-            if (hit.collider != null)
+            foreach (RaycastHit2D hit in hits)
             {
-                //Debug.Log(hit.collider.gameObject.name);
-                linePoints.Insert(linePoints.Count - 1, endPos.position);
-                Vector2 normal = hit.normal;
-                currentDirection = Vector2.Reflect(currentDirection, normal).normalized;
-                endPos.up = currentDirection;
-                //長さの最大値の残り長さをここで反映
-                if (linePoints.Count >= 3)
+                if (hit.collider.gameObject != preWall)
                 {
-                    canMagnitude -= (linePoints[linePoints.Count - 2] - linePoints[linePoints.Count - 3]).magnitude;
-                }
+                    Vector3 endPos = hit.point;
+                    Vector2 reflectDirection = Vector2.Reflect(direction, hit.normal);
 
-            }
+                    line.SetPosition(0, origin);
+                    line.SetPosition(1, endPos);
+                    preWall = hit.collider.gameObject;
 
-            Vector3 newPos = currentDirection * tongueSpeed * Time.deltaTime;
-            endPos.position += new Vector3(newPos.x,newPos.y, 0);
+                    creat(endPos, reflectDirection, ++n);
 
-            //長さ制限の処理
-            Vector3 limitPos = endPos.position - linePoints[linePoints.Count - 2];
-            if (limitPos.magnitude > canMagnitude)
-            {
-                limitPos *= canMagnitude / limitPos.magnitude;
-                endPos.position = limitPos + linePoints[linePoints.Count - 2];
-            }
-
-            linePoints[linePoints.Count - 1] = endPos.position;
-            UpdateLine();
-        }
-        else if (backFlag) //縮んで行くとき
-        {
-            //１つ前の頂点or起点へ戻っていくようにする（そのまま再度伸びることもできるように
-
-            float distance = Vector2.Distance(endPos.position, linePoints[linePoints.Count - 2]);
-            if (distance < 0.5f)
-            {
-                endPos.position = linePoints[linePoints.Count - 2];              
-                if (linePoints.Count > 2) //反射が１個でも残っているとき
-                {
-                    endPos.up = (endPos.position - linePoints[linePoints.Count - 3]).normalized;
-                    canMagnitude += (linePoints[linePoints.Count - 2] - linePoints[linePoints.Count - 3]).magnitude;
-                    linePoints.RemoveAt(linePoints.Count - 2);
-                }
-                else
-                {
-                    Debug.Log("戻った！");
-                    backFlag = false;
-                    edgeLine.backFlag = false;
+                    break;
                 }
             }
-            else if(distance >= 20.0f)
-            {
-                Debug.Log("異常値まで離れています");
-                endPos.position = linePoints[linePoints.Count - 2];
-                if (linePoints.Count > 2) //反射が１個でも残っているとき
-                {
-                    endPos.up = (endPos.position - linePoints[linePoints.Count - 3]).normalized;
-                    canMagnitude += (linePoints[linePoints.Count - 2] - linePoints[linePoints.Count - 3]).magnitude;
-                    linePoints.RemoveAt(linePoints.Count - 2);
-                }
-                else
-                {
-                    Debug.Log("緊急で戻ります");
-                    backFlag = false;
-                    edgeLine.backFlag = false;
-                }
-            }
-        }
-        else //起点に戻ったとき、伸ばしていないとき
-        {
-            //endPos.position = startPos.position;
-            currentDirection = (endPos.position - startPos.position).normalized;
-            canMagnitude = maxMagnitude;
-
-            //とばして戻った場合
-            if (linePoints.Count > 2)
-            {
-                Debug.Log("とばして戻ってます");
-                for (int i = linePoints.Count; i > 2; i--)
-                {
-                    linePoints.RemoveAt(linePoints.Count - 2);
-                }
-            }
-            
-        }
-
-        linePoints[0] = startPos.position;
-        linePoints[linePoints.Count - 1] = endPos.position;
-        UpdateLine();
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Damage")
-        {
-            Debug.Log("ミス");
-            Destroy(collision.gameObject);
-        }
-        else if (collision.gameObject.tag == "Item")
-        {
-            Debug.Log("うますぎる");
-            //float score += collision.gameObject.Item().score;//アイテムのスコアを受け取るようにする
-            Item ItemScript = collision.gameObject.GetComponent<Item>();
-            getCount += 1;
-            tongueScore += ItemScript.scoreValue;
-            Destroy(collision.gameObject);
         }
     }
-
-    //ここで、LinRendererとedgeColliderの変化を反映させている
-    void UpdateLine()
-    {
-        //線の頂点の数を反映させる
-        lineRenderer.positionCount = linePoints.Count; 
-        //ToArrayメソッドで、リストから配列へ変換させた上で引数として与えている。ここでLineRenderの頂点の座標を反映させている
-        lineRenderer.SetPositions(linePoints.ToArray()); 
-
-        // EdgeCollider のために Vector2 に変換
-        List<Vector2> edgePoints = new List<Vector2>();
-        foreach (var point in linePoints)
-        {
-            edgePoints.Add(new Vector2(point.x, point.y));
-        }
-        //ここでedgeColliderの頂点の座標を反映させている
-        edgeCollider.SetPoints(edgePoints);
-
-    }
-
-
 }
